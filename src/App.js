cda2837b7c34b64b7a43c3195ac4b7d6b6dc7efd
Long Nguyen D. [4] <long.nguyen4@monstar-lab.com>
2021-06-14 22:17:32 +0700

    BaiTap2

--- src/App.js
+++ src/App.js
@@ -2,19 +2,48 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './Header.js';
 import Footer from './Footer.js';
+import Circle from './Circle.js';
+import React from 'react';
 
+class Main extends React.Component {
+  constructor(props) {
+      super(props);
+      this.state = {
+          page: 'home',
+      };
+  }
+  handleChangePage(page) {
+    this.setState({page});
+  }
+
+  render() {
+      const {page} = this.state;
+      const circle = {
+        height : "100px",
+        width : "100px",
+        borderRadius : "50%",
+        backgroundColor : "yellow",
+    }
+      return (
+          <main className="App-header">
+              <div className="list-btn">
+                  <button onClick={() => this.handleChangePage("home")} className="home-btn">Home</button>
+                  <button onClick={() => this.handleChangePage("about")} className="about-btn">About</button>
+              </div>
+              <ul className="list-circle">
+                <li>{ page === "home" && <Circle styleCircle={circle} number={Math.floor(50)}/> }</li>
+                <li>{ page === "home" && <Circle styleCircle={circle} number={Math.floor(40)}/> }</li>
+                <li>{ page === "home" && <Circle styleCircle={circle} number={Math.floor(30)}/> }</li>
+              </ul>
+          </main>
+      )
+  }
+}
 function App() {
   return (
     <>
     <Header/>
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello React!
-        </p>
-      </header>
-    </div>
+    <Main/>
     <Footer/>
     </>
   );
